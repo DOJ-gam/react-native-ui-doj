c2e5968e9717b1e448ba097a1cb6ff8da3526c8d
doj-gam <omarjeng999@gmail.com>
2022-03-26 23:42:20 +0000

    add Dimage

---
+++ src/image/Dimage.js
@@ -0,0 +1,95 @@
+import { StyleSheet, Image } from "react-native";
+import React from "react";
+
+const Dimage = (props) => {
+  const {
+    image, //
+    flex, //
+    items, //
+    content, //
+    bbw, //
+    btw, //
+    bbc, //
+    btc, //
+    blw, //
+    brw, //
+    blc, //
+    brc, //
+    h,
+    w,
+    m,
+    mb,
+    mt,
+    my,
+    mx,
+    ml,
+    mr,
+    p,
+    pt,
+    pb,
+    px,
+    py,
+    pl,
+    pr,
+
+    bg,
+    br,
+    bw,
+    bc,
+    shadow,
+    children,
+    style,
+    ...rest
+  } = props;
+  return (
+    <Image
+      source={image}
+      style={{
+        flexDirection: flex && flex,
+        alignItems: items && items,
+        justifyContent: content && content,
+
+        margin: m && m,
+        marginVertical: my && my,
+        marginHorizontal: mx && mx,
+        marginTop: mt && mt,
+        marginBottom: mb && mb,
+        marginLeft: ml && ml,
+        marginRight: mr && mr,
+
+        padding: p && p,
+        paddingVertical: py && py,
+        paddingHorizontal: px && px,
+        paddingTop: pt && pt,
+        paddingBottom: pb && pb,
+        paddingLeft: pl && pl,
+        paddingRight: pr && pr,
+
+        borderWidth: bw && bw,
+        borderColor: bc && bc,
+        borderTopWidth: btw && btw,
+        borderTopColor: btc && btc,
+        borderBottomWidth: bbw && bbw,
+        borderBottomColor: bbc && bbc,
+        borderLeftWidth: blw && blw,
+        borderLeftColor: blc && blc,
+        borderRightWidth: brw && brw,
+        borderRightColor: brc && brc,
+
+        height: h && h,
+        width: w && w,
+
+        backgroundColor: bg && bg,
+        borderRadius: br && br,
+
+        elevation: shadow && shadow,
+
+        ...style,
+      }}
+    />
+  );
+};
+
+export default Dimage;
+
+const styles = StyleSheet.create({});
